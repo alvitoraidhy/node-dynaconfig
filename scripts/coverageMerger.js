784ff1d8020a1c0c6c38cfac987a2bd0113a0345
Alvito Raidhy <alvito.raidhy21@gmail.com>
2021-07-13 18:46:30 +0700

    Fix coverage merger script

--- scripts/coverageMerger.js
+++ scripts/coverageMerger.js
@@ -16,6 +16,11 @@ const getLcovFiles = function (src) {
 (async function(){
   const files = await getLcovFiles('coverage');
   const mergedReport = files.reduce((mergedReport, currFile) => mergedReport += fs.readFileSync(currFile), '');
+  try {
+    await fs.promises.mkdir(path.resolve('./coverage'));
+  } catch (err) {
+    if (err.code !== "EEXIST") throw err;
+  }
   await fs.writeFile(path.resolve('./coverage/lcov.info'), mergedReport, (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
